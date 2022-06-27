52a2f061da0558994b7edf03d78a5a5cd93125a6
bca18 <bharateeshwork@gmail.com>
2022-06-27 23:14:01 +0530

    Deleted Pavan

--- Pavan_contrib/hospitalmanagementsystem/hospital/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def homepage(request):
-	return render(request,'index.html')
-
-def aboutpage(request):
-	return render(request,'about.html')
